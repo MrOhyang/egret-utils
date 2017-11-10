ca5b90acf1ac51cf4d4d0505c174045d9063779e
Ouyang Yibin <ohyang94e@163.com>
2017-11-10 10:04:44 +0800

    添加 http网络请求类

--- README.md
+++ README.md
@@ -1,14 +1,39 @@
 # egret-utils
 基于白鹭引擎 egret v5.0.9 的工具类库
 
+
+
 ## 1. ObjectPool.ts 对象池管理类
 使用方法：
 
-1. ObjectPool.getInstance().createObject(classFactory: any) // 创建一个对象实例
-2. ObjectPool.getInstance().deleteObject(obj: Object)  // 回收一个对象实例
+```javascript
+// 创建一个对象实例
+let obj = ObjectPool.getInstance().createObject(classFactory: any);
+
+// 回收一个对象实例
+let obj = ObjectPool.getInstance().deleteObject(obj: Object);
+```
 
 > 注意事项：在使用该类的方法的时候，需要要求 classFactory 类中拥有静态成员属性 key 值来进行区分创建和回收存储不同种类的对象。
 > 如果需要查看详细的设计细节或具体的使用方法，[**请戳此处链接**](http://a8ccce0e.wiz03.com/share/s/2EPcUe16jknd21LCSr0jir5o1Uo2SA2TUQKH2PyOcZ0XXImI)。
+
+
+
+## 2. Http.ts 网络请求封装
+使用方法：
+
+```javascript
+// 1. async/await
+let res = await new Http().post('/api/test', postData);
+
+// 2. promise
+new Http().post('/api/test', postData).then(r => {
+  // do something...
+}).catch(e => {});
+```
+
+> 注意事项：必须使用 new 来创建一个请求。默认超时时间为30s，可直接修改默认值，或者新建方法参数来修改默认值。
+
 
 
 
---
+++ src/Http.ts
@@ -0,0 +1,134 @@
+/**
+ * 基于 egret.HttpRequest 封装的 Http 类，主要用来进行网络请求封装
+ * post/get 方法返回的都是 promise 对象，可以支持新的 promise/await 新的语法。
+ * 
+ * 使用方法：
+ * let res = await new Http().post('/api/test', {id: 1});
+ */
+class Http {
+    /**
+     * egert 的请求对象实例
+     */
+    private request: egret.HttpRequest;
+
+    /**
+     * 设置的 Http 请求超时时间, 默认30s
+     */
+    public static timeout: number = 30000;
+
+    /**
+     * 构造函数
+     */
+    public constructor() {
+        this.request = new egret.HttpRequest();
+        this.request.responseType = egret.HttpResponseType.TEXT;
+        return this;
+    }
+
+    /**
+     * post 方法
+     * @param url 一个用来包含发送请求的 url 字符串
+     * @param param 发送到服务器的数据
+     */
+    public post(url: string, param: Object = {}) {
+        let timer = null,
+            timeout = Http.timeout;
+
+        return new Promise((resolve, reject) => {
+            this.request.open(this.getUrl(url), egret.HttpMethod.POST);
+            this.request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            this.request.send(this.formatPostData(param));
+            this.request.addEventListener(egret.Event.COMPLETE, (e: egret.Event) => {
+                let request = <egret.HttpRequest>e.currentTarget;
+                window.clearTimeout(timer);
+                resolve(JSON.parse(request.response));
+            }, this);
+            this.request.addEventListener(egret.IOErrorEvent.IO_ERROR, (e: egret.IOErrorEvent) => {
+                window.clearTimeout(timer);
+                reject(e);
+            }, this);
+            window.setTimeout(() => {
+                reject({
+                    msg: `该链接已超时: ${timeout}`,
+                    url,
+                    param
+                });
+            }, timeout);
+        });
+    }
+
+    /**
+     * get 方法
+     * @param url 一个用来包含发送请求的 url 字符串
+     * @param param 发送到服务器的数据
+     */
+    public get(url: string, param: Object = {}) {
+        let timer = null,
+            timeout = Http.timeout;
+
+        return new Promise((resolve, reject) => {
+            let getData = this.formatPostData(param),
+                real_url = this.getUrl(url);
+            
+            if (getData != '') {
+                real_url += `?${getData}`;
+            }
+            this.request.open(real_url, egret.HttpMethod.POST);
+            this.request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            this.request.send();
+            this.request.addEventListener(egret.Event.COMPLETE, (e: egret.Event) => {
+                let request = <egret.HttpRequest>e.currentTarget;
+                window.clearTimeout(timer);
+                resolve(JSON.parse(request.response));
+            }, this);
+            this.request.addEventListener(egret.IOErrorEvent.IO_ERROR, (e: egret.IOErrorEvent) => {
+                window.clearTimeout(timer);
+                reject(e);
+            }, this);
+            timer = window.setTimeout(() => {
+                reject({
+                    msg: `该链接已超时: ${timeout}`,
+                    url,
+                    param
+                });
+            }, timeout);
+        });
+    }
+
+    /**
+     * 获取完整 url 路径，（主要用来区别本地环境还是什么环境的）
+     * @param url 一个用来包含发送请求的 url 字符串
+     */
+    private getUrl(url: string): string {
+        if (/^((127.0.0.1)|(yes.com))/.test(window.location.host)) {
+            return `http://127.0.0.1:8889${url}`;
+        }
+        return url;
+    }
+   
+    /**
+     * 格式化传参, eg: {p1: a, p2: b}  ==>  'p1=a&p2=b'
+     * @param param 发送到服务器的数据
+     */
+    private formatPostData(param: Object): string {
+        let arr = [],
+            val = '';
+
+        for (let key in param) {
+            // 如果是数组，或者是对象，则进行 JSON.stringify
+            if (typeof param[key] == 'object') {
+                try {
+                    val = JSON.stringify(param[key]);
+                } catch(e) {
+                    console.log(`Http.formatPostData 参数异常: `, param);
+                    val = '';
+                }
+                arr.push(`${key}=${val}`);
+            } else {
+                arr.push(`${key}=${param[key]}`);
+            }
+        }
+        if (arr.length <= 0) return undefined;
+        return arr.join('&');
+    }
+}
